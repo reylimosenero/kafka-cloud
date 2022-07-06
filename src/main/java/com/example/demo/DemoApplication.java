35f6605b7bdf4fe723ee9ead6d702df864b8eb66
rey.limosenero <rey.limosenero@softvision.com>
2022-07-06 14:25:54 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,54 @@
+package com.example.demo;
+
+import com.github.javafaker.Faker;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.util.function.Tuple2;
+
+import java.time.Duration;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+@RequiredArgsConstructor
+@Component
+class Producer{
+
+	private final KafkaTemplate<Integer,String> template;
+
+	Faker faker;
+	@EventListener(ApplicationStartedEvent.class)
+	public void generate(){
+		faker = Faker.instance();
+		Flux<Long> interval = Flux.interval(Duration.ofMillis(1_000));
+		Flux<String> objectFlux = Flux.fromStream(Stream.generate(new Supplier<String>() {
+			@Override
+			public String get() {
+				return faker.hobbit().quote();
+			}
+		}));
+
+		Flux.zip(interval,objectFlux).map(new Function<Tuple2<Long, String>, Object>() {
+			@Override
+			public Object apply(final Tuple2<Long, String> it) {
+				return template.send("mini-proj-topic", faker.random().nextInt(42), it.getT2());
+			}
+		}).blockLast();
+
+	}
+
+}
